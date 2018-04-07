f3cf0a3646c4460835eced14e2bb9cde78d56782
Giacomo Mammarella <giacu92@gmail.com>
2018-04-07 16:05:40 +0200

    Upload project libraries

---
+++ Libraries/XPT2046_Touchscreen-master/examples/ILI9488Test/ILI9488Test.ino
@@ -0,0 +1,69 @@
+#include <ILI9488.h>
+#include "Arial14.h" // from ILI9488_t3
+#include <XPT2046_Touchscreen.h>
+#include <SPI.h>
+
+#define CS_PIN  8
+#define TFT_DC  9
+#define TFT_CS 10
+#define TFT_RST 13
+// MOSI=11, MISO=12, SCK=13
+
+XPT2046_Touchscreen ts(CS_PIN);
+#define TIRQ_PIN  2
+//XPT2046_Touchscreen ts(CS_PIN);  // Param 2 - NULL - No interrupts
+//XPT2046_Touchscreen ts(CS_PIN, 255);  // Param 2 - 255 - No interrupts
+//XPT2046_Touchscreen ts(CS_PIN, TIRQ_PIN);  // Param 2 - Touch IRQ Pin - interrupt enabled polling
+
+ILI9488 tft = ILI9488(TFT_CS, TFT_DC, TFT_RST);
+
+void setup() {
+  Serial.begin(38400);
+  tft.begin();
+  tft.setRotation(1);
+  tft.fillScreen(ILI9488_BLACK);
+  ts.begin();
+  while (!Serial && (millis() <= 1000));
+}
+
+boolean wastouched = true;
+
+void loop() {
+  boolean istouched = ts.touched();
+  if (istouched) {
+    TS_Point p = ts.getPoint();
+    if (!wastouched) {
+      tft.fillScreen(ILI9488_BLACK);
+      tft.setTextColor(ILI9488_YELLOW);
+
+      tft.setCursor(60, 80);
+      tft.print("Touch");
+    }
+    tft.fillRect(100, 150, 140, 60, ILI9488_BLACK);
+    tft.setTextColor(ILI9488_GREEN);
+
+    tft.setCursor(100, 150);
+    tft.print("X = ");
+    tft.print(p.x);
+    tft.setCursor(100, 180);
+    tft.print("Y = ");
+    tft.print(p.y);
+    Serial.print(", x = ");
+    Serial.print(p.x);
+    Serial.print(", y = ");
+    Serial.println(p.y);
+  } else {
+    if (wastouched) {
+      tft.fillScreen(ILI9488_BLACK);
+      tft.setTextColor(ILI9488_RED);
+
+      tft.setCursor(120, 50);
+      tft.print("No");
+      tft.setCursor(80, 120);
+      tft.print("Touch");
+    }
+    Serial.println("no touch");
+  }
+  wastouched = istouched;
+  delay(100);
+}
